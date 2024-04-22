bc30ef2dd9f2a7defb6bbe346ff6d07d5a7fd801
Robert Tian <robertian23@berkeley.edu>
2024-04-22 12:43:03 -0700

    add stopwatch

---
+++ Assets/Stopwatch.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Stopwatch : MonoBehaviour
+{
+    private bool _timerActive;
+    private float _currentTime;
+    [SerializeField] private TMP_Text _text;
+
+    public void Start()
+    {
+        _currentTime = 0;      
+    }
+
+    public void Update()
+    {
+        if (_timerActive)
+        {
+            _currentTime = _currentTime + Time.deltaTime;
+        }
+
+        TimeSpan time = TimeSpan.FromSeconds(_currentTime);
+        _text.text = time.Minutes.ToString() + ":" + time.Seconds.ToString() + ":" + time.Milliseconds.ToString();
+    }
+
+    public void StartStopwatch()
+    {
+        _timerActive = true;
+    }
+    public void StopStopwatch()
+    {
+        _timerActive = false;
+    }
+    public void ResetStopwatch()
+    {
+        _timerActive = false;
+        _currentTime = 0;
+    }
+}
